ac38c6cb4f1a7d5e9aa76b921a57413a2206adc7
munibafareed <fmuniba34@gmail.com>
2021-11-14 16:01:01 -0800

    work complete

---
+++ app.js
@@ -0,0 +1,72 @@
+// string mathod question # 1,,2,,,
+
+// var firstName = prompt("enter your phone firstName");
+// var lastName = prompt("enter your last phone name");
+// var fullName = firstName + lastName ;
+// console.log(fullName)
+// console.log(fullName.length)
+
+
+// Q # 3,,,,,index of,,,,
+
+// var name = "pakistani";
+// console.log(name.indexOf("n"))
+
+
+// Q # 4 ,,indexof -1,,,
+
+// var value = "hello world";
+// console.log(value.indexOf("i"))
+
+// Q # 7,,,replace,,,,
+ 
+// var city = "hyderabad";
+// city = city.replace("hyderabad" , "islamabad");
+// console.log(city)
+
+// Q # 8,,,replce(globale),,,
+ 
+
+//  var message ="Ali and Sami are best friends. They play cricket and football together.";
+//  message = message.replace(/and/g , "&");
+//  console.log(message)
+
+// Q # 9,,,value and type,,,
+
+// var name = "472";
+// console.log(typeof(name))
+
+// var num = 472;
+// console.log(typeof(num))
+
+//  Q # 12,,,,,split method,,,
+
+// var string = "35.36" ;
+// console.log(string);
+// console.log(string.split(".").join(""));
+
+// Q # 10 upper case,,,,
+
+
+
+
+
+
+
+
+
+
+
+// math methodd,,,quiestion # 1,,,,
+// round of
+
+// var num = 3.45214 ;
+// var roundValue = Math.round(num);
+// console.log(roundValue)
+
+// floor value
+
+var num = 3.45214 ;
+var roundValue = Math.floor(num);
+console.log(r)
+
